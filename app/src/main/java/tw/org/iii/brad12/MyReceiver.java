37a9480c4052811f99f0bcefa0db3836a6fd5b42
brad <brad@brad.tw>
2017-01-22 14:54:28 +0800

    v2

---
+++ app/src/main/java/tw/org/iii/brad12/MyReceiver.java
@@ -0,0 +1,16 @@
+package tw.org.iii.brad12;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class MyReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        //throw new UnsupportedOperationException("Not yet implemented");
+        Log.v("brad", "onReceive");
+    }
+}
